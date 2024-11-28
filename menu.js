365322cb31ef41d881b0b1334c6240d9879c061d
HaninKhalil <haninkhalil222@gmail.com>
2024-11-28 02:46:01 +0200

    ADD Drop Menu JS Filtration

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $('.categories_menu div').on('click', function () {
+    $('.categories_menu .div').on('click', function () {
         let category = $(this).find('span').text().toLowerCase();
         filterCards(category);
     });
@@ -14,10 +14,25 @@ $(document).ready(function () {
         }
     }
 
+    $('.dropdown-content a').on('click', function (event) { // Filtering through dropdown menu
+        event.preventDefault(); // Prevent the default anchor click behavior
+        let drop = $(this).parent().data('category'); // Get the category from the parent div
+        filterCards(drop);
+    });
+    
+    function filterCards(drop) {
+        if (drop === 'all') {
+            $('.card').show(); // Show all cards
+        } else {
+            $('.card').hide(); // Hide all cards
+            $('.card[data-category="' + drop + '"]').show(); // Show only selected category cards
+        }
+    }
+
     $('.wishlist').on('click', function () // wishlist feature
     {
         $(this).find('.wishlist_black').toggle();
         $(this).find('.wishlist_red ').toggle();
     });
-    
+
 });
